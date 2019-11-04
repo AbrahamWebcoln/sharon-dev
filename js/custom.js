0d66458843a9afafd56110f62ceffc83fc1b726b
FrazettaSilke <abraham@abaracus.com>
2019-11-04 09:49:30 -0600

    formatting and jquery fixes

--- js/custom.js
+++ js/custom.js
@@ -1,151 +1,64 @@
+(function($) {
 
-(function($) { 
-	"use strict";
-	
-	(function($) {
-$(function() {
-jQuery('#loopedSlider').prepend("<a href='#' class='previous'>&lt;</a><a href='#' class='next'>&gt;</a>");
-	jQuery('#loopedSlider').loopedSlider({
-		autoHeight: 500
-	});
-});
-});
+  // for filter
+  $(document).ready(function() {
+      var $container = $('.portfolioContainer');
+      $container.isotope({
+          filter: '*',
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+      });
+      $('.portfolioFilter a').click(function() {
+          $('.portfolioFilter .current').removeClass('current');
+          $(this).addClass('current');
 
-
-
-// for banner height js
-var windowWidth = $(window).width();
-    var windowHeight =$(window).height();
-    $('.banner').css({'width':windowWidth ,'height':windowHeight -"60" });
-	
-	
-
-
-// for portfoli filter jquary
-$(window).load(function(){
-    var $container = $('.portfolioContainer');
-    $container.isotope({
-        filter: '*',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear',
-            queue: false
-        }
-    });
- 
-    $('.portfolioFilter a').click(function(){
-        $('.portfolioFilter .current').removeClass('current');
-        $(this).addClass('current');
- 
-        var selector = $(this).attr('data-filter');
-        $container.isotope({
-            filter: selector,
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-         });
-         return false;
-    }); 
-});
-
-
-
-
-// for portfoli lightbox jquary
-jQuery(function($) {
-	var $chosenSheet,
-	$stylesheets = $( "a[id^=theme-]" );
-	
-	// run rlightbox
-	$( ".lb" ).rlightbox();
-	$( ".lb_title-overwritten" ).rlightbox({overwriteTitle: true});
-});
-
-
-
-
-
-// for skill chat jquary
-$(document).ready(function(e) {
-//var windowBottom = $(window).height();
-var index=0;
-if(index==0){	
-			
-  $('.chart').easyPieChart({
-    easing: 'easeOutBounce',
-    onStep: function(from, to, percent) {
-      $(this.el).find('.percent').text(Math.round(percent));
-    }
+          var selector = $(this).attr('data-filter');
+          $container.isotope({
+              filter: selector,
+              animationOptions: {
+                  duration: 750,
+                  easing: 'linear',
+                  queue: false
+              }
+          });
+          return false;
+      });
   });
-  
-}
-index++;
-$(document).scroll(function(){
-//	var top = $('.technical').height()-$(window).scrollTop();
-//	console.log(top)
-//	if(top<-300){
-//	}
-})
-//console.log(nagativeValue)
-});
 
+  // Nav bar
+  $(document).ready(function(e) {
+      $(document).on("click", ".navbar .menuItem", function(t) {
+          t.preventDefault();
+          var $href = $(this).find('a').attr('href');
+          $('html, body').animate({
+                  scrollTop: $($href).offset().top,
+              },
+              500,
+              'linear'
+          )
 
-
-
-// Somth page scroll
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top -60
-        }, 1000);
-        return false;
-      }
-    }
+      });
   });
-});
 
+  //Creds and Evals
+  $(document).ready(function(e) {
+      $(document).on("click", ".lightCon .hoverBox", function(t) {
+          t.preventDefault();
+          var $href = $(this).find('a').attr('href');
 
-
-
-// chart loding
-$(window).load(function() {
-	
-	var chart = window.chart = $('.chart').data('easyPieChart');
-	$('.js_update').on('click', function() {
-		chart.update(Math.random()*100);
-	});
-});
-
-
-
-
-
-//jQuery
-$(window).load(function() {    
-
-        var theWindow        = $(window),
-            $bg              = $(".bannerImg");
-            //aspectRatio      = $bg.width() / $bg.height();
-
-        function resizeBg() {
-                if ( theWindow.width() < theWindow.height() ) {
-                    $bg
-                        .removeClass()
-                        .addClass('bgheight');
-                } else {
-                    $bg
-                        .removeClass()
-                        .addClass('bgwidth');
-                }
-        }
-
-        theWindow.resize(resizeBg).trigger("resize");
-
-});
+          if ($href.indexOf('#') > -1) {
+              $('html, body').animate({
+                      scrollTop: $($href).offset().top,
+                  },
+                  500,
+                  'linear'
+              )
+          } else {
+              window.location = $href;
+          }
+      });
+  });
 }(jQuery));
